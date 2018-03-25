54d9b3a9ac6ae9400f5475bee3d55ac6354923e6
Brian Rodriguez <thatbrod@gmail.com>
2018-03-25 17:45:06 -0400

    Rename tfmr to trans.

--- rn2md/transformers_test.py
+++ rn2md/transformers_test.py
@@ -9,93 +9,94 @@ import transformers
 class ItalicTransformerTest(unittest.TestCase):
 
     def setUp(self):
-        self.tfmr = transformers.ItalicTransformer(); next(self.tfmr)
+        self.trans = transformers.ItalicTransformer(); next(self.trans)
 
     def testTransformation(self):
         self.assertEqual(
-            self.tfmr.send('Text with //italicized// content.'),
+            self.trans.send('Text with //italicized// content.'),
             'Text with _italicized_ content.')
 
     def testIgnoresNonPairedMarkers(self):
         self.assertEqual(
-            self.tfmr.send('//italic1//, //italic2//, unused //'),
+            self.trans.send('//italic1//, //italic2//, unused //'),
             '_italic1_, _italic2_, unused //')
 
     def testIgnoresUrls(self):
         self.assertEqual(
-            self.tfmr.send('http://github.com/brianrodri'),
+            self.trans.send('http://github.com/brianrodri'),
             'http://github.com/brianrodri')
 
     def testIgnoresBacktickedData(self):
         self.assertEqual(
-            self.tfmr.send('//italic//, `//escaped italic//`'),
+            self.trans.send('//italic//, `//escaped italic//`'),
             '_italic_, `//escaped italic//`')
 
 
 class LinkTransformerTest(unittest.TestCase):
 
     def setUp(self):
-        self.tfmr = transformers.LinkTransformer(); next(self.tfmr)
+        self.trans = transformers.LinkTransformer(); next(self.trans)
 
     def testTransformation(self):
         self.assertEqual(
-            self.tfmr.send('[[sample text ""go/somewhere""]]'),
+            self.trans.send('[[sample text ""go/somewhere""]]'),
             '[sample text](go/somewhere)')
 
 
 class StrikethroughTransformerTest(unittest.TestCase):
 
     def setUp(self):
-        self.tfmr = transformers.StrikethroughTransformer(); next(self.tfmr)
+        self.trans = transformers.StrikethroughTransformer(); next(self.trans)
 
     def testTransformation(self):
-        self.assertEqual(self.tfmr.send('--text--'), '**OBSOLETE**(text)')
+        self.assertEqual(self.trans.send('--text--'), '**OBSOLETE**(text)')
 
     def testPunctuationGetsStripped(self):
         self.assertEqual(
-            self.tfmr.send('--a complete sentence.--'),
+            self.trans.send('--a complete sentence.--'),
             '**OBSOLETE**(a complete sentence)')
 
     def testIgnoresNonPairedMarkers(self):
         self.assertEqual(
-            self.tfmr.send('--changed--, --this too--, not here--or here.'),
+            self.trans.send('--changed--, --this too--, not here--or here.'),
             '**OBSOLETE**(changed), **OBSOLETE**(this too), not here--or here.')
 
     def testIgnoresUrls(self):
         self.assertEqual(
-            self.tfmr.send('http://do/something--weird'),
+            self.trans.send('http://do/something--weird'),
             'http://do/something--weird')
 
     def testIgnoresBacktickedData(self):
         self.assertEqual(
-            self.tfmr.send('--hit--, `--not hit--`'),
+            self.trans.send('--hit--, `--not hit--`'),
             '**OBSOLETE**(hit), `--not hit--`')
 
 
 class HeaderTransformerTest(unittest.TestCase):
 
     def setUp(self):
-        self.tfmr = transformers.HeaderTransformer(); next(self.tfmr)
+        self.trans = transformers.HeaderTransformer(); next(self.trans)
 
     def testTransformation(self):
-        self.assertEqual(self.tfmr.send('=Level One='), '# Level One')
-        self.assertEqual(self.tfmr.send('===Level Three==='), '### Level Three')
+        self.assertEqual(self.trans.send('=Level One='), '# Level One')
+        self.assertEqual(
+            self.trans.send('===Level Three==='), '### Level Three')
 
     def testBaseLevelIsRespected(self):
-        tfmr = transformers.HeaderTransformer(base_level=2); next(tfmr)
-        self.assertEqual(tfmr.send('===Only 3==='), '##### Only 3')
+        trans = transformers.HeaderTransformer(base_level=2); next(trans)
+        self.assertEqual(trans.send('===Only 3==='), '##### Only 3')
 
     def testInnerMarkersAreIgnored(self):
         self.assertEqual(
-            self.tfmr.send('Not at =start= of text'),
+            self.trans.send('Not at =start= of text'),
             'Not at =start= of text')
 
     def testOnlyMarkersAreIgnored(self):
-        self.assertEqual(self.tfmr.send('=' * 6), '=' * 6)
-        self.assertEqual(self.tfmr.send('=' * 7), '=' * 7)
+        self.assertEqual(self.trans.send('=' * 6), '=' * 6)
+        self.assertEqual(self.trans.send('=' * 7), '=' * 7)
 
     def testUnBalancedMarkersAreIgnored(self):
-        self.assertEqual(self.tfmr.send('==Unbalanced==='), '==Unbalanced===')
+        self.assertEqual(self.trans.send('==Unbalanced==='), '==Unbalanced===')
 
 
 if __name__ == '__main__':
