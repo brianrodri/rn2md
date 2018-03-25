1fc14d547a584a819b29bb7becc5014e846980d7
Brian Rodriguez <thatbrod@gmail.com>
2018-03-25 14:28:04 -0400

    Add italics transformer coverage.

---
+++ rn2md/transformers_test.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+"""Test cases for the transformers module."""
+import unittest
+
+import transformers
+
+
+class ItalicTransformerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.transformer = transformers.ItalicTransformer()
+        next(self.transformer)
+
+    def testTransformation(self):
+        self.assertEqual(
+            self.transformer.send('Text with //italicized// content.'),
+            'Text with _italicized_ content.')
+
+    def testIgnoresNonPairedMarkers(self):
+        self.assertEqual(
+            self.transformer.send('//italic1//, //italic2//, unused //'),
+            '_italic1_, _italic2_, unused //')
+
+    def testIgnoresUrls(self):
+        self.assertEqual(
+            self.transformer.send('http://github.com/brianrodri'),
+            'http://github.com/brianrodri')
+
+    def testIgnoresBacktickedData(self):
+        self.assertEqual(
+            self.transformer.send('//italic//, `//escaped italic//`'),
+            '_italic_, `//escaped italic//`')
+
+
+if __name__ == '__main__':
+    unittest.main()
