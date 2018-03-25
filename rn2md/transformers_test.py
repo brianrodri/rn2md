7b45d8949dbc829c93171608e4059efb3150ed37
Brian Rodriguez <thatbrod@gmail.com>
2018-03-25 14:34:21 -0400

    Rename to tfmr.

--- rn2md/transformers_test.py
+++ rn2md/transformers_test.py
@@ -9,39 +9,39 @@ import transformers
 class ItalicTransformerTest(unittest.TestCase):
 
     def setUp(self):
-        self.transformer = transformers.ItalicTransformer()
-        next(self.transformer)
+        self.tfmr = transformers.ItalicTransformer()
+        next(self.tfmr)
 
     def testTransformation(self):
         self.assertEqual(
-            self.transformer.send('Text with //italicized// content.'),
+            self.tfmr.send('Text with //italicized// content.'),
             'Text with _italicized_ content.')
 
     def testIgnoresNonPairedMarkers(self):
         self.assertEqual(
-            self.transformer.send('//italic1//, //italic2//, unused //'),
+            self.tfmr.send('//italic1//, //italic2//, unused //'),
             '_italic1_, _italic2_, unused //')
 
     def testIgnoresUrls(self):
         self.assertEqual(
-            self.transformer.send('http://github.com/brianrodri'),
+            self.tfmr.send('http://github.com/brianrodri'),
             'http://github.com/brianrodri')
 
     def testIgnoresBacktickedData(self):
         self.assertEqual(
-            self.transformer.send('//italic//, `//escaped italic//`'),
+            self.tfmr.send('//italic//, `//escaped italic//`'),
             '_italic_, `//escaped italic//`')
 
 
 class LinkTransformer(unittest.TestCase):
 
     def setUp(self):
-        self.transformer = transformers.LinkTransformer()
-        next(self.transformer)
+        self.tfmr = transformers.LinkTransformer()
+        next(self.tfmr)
 
     def testTransformation(self):
         self.assertEqual(
-            self.transformer.send('[[sample text ""go/somewhere""]]'),
+            self.tfmr.send('[[sample text ""go/somewhere""]]'),
             '[sample text](go/somewhere)')
 
 
