91f69d2f5e8470fca79940ebc0be8db45fd26364
Brian Rodriguez <thatbrod@gmail.com>
2018-03-25 09:20:17 -0400

    Update setup.py to become useful.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from distutils.core import setup
 
 setup(
-    name="RednotebookUtils",
-    version="0.1dev",
-    packages=["rednotebookutils",],
-    license="???",
-    long_description=open("README.txt").read(),
+    name='RedNotebookToMarkDown',
+    version='0.2dev',
+    description='Utility to print RedNotebook data in the MarkDown format.',
+    long_description=open('README.txt').read(),
+    url='http://github.com/brianrodri/RednotebookToMarkdown',
+    author='Brian Rodriguez',
+    author_email='brian@brianrodri.com',
+    license='MIT',
+    packages=['rn2md'],
+    install_requires=[
+        'dateutil',
+        'defaultlist',
+        'freezegun',
+        'isoweek',
+        'parsedatetime',
+        'pyfakefs',
+        'yaml',
+    ],
 )
