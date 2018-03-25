160f10517df4eec0d03e18fac30d5a47a87e4450
Brian Rodriguez <thatbrod@gmail.com>
2018-03-25 00:05:30 -0400

    Use prettier list-literal formatting.

--- rn2md/util_test.py
+++ rn2md/util_test.py
@@ -22,70 +22,79 @@ class ParseDatesTest(unittest.TestCase):
 
     @freezegun.freeze_time('Mar 24th, 2018 12pm')
     def testToday(self):
-        self.assertListEqual(
-            util.ParseDates('today'), [StrToDate('Mar 24th, 2018')])
+        self.assertListEqual(util.ParseDates('today'), [
+            StrToDate('Mar 24th, 2018'),
+        ])
 
     @freezegun.freeze_time('Mar 23rd, 2018 12pm')
     def testThisWeek(self):
-        self.assertListEqual(
-            util.ParseDates('this week'),
-            [StrToDate('Mon Mar 19th, 2018'), StrToDate('Tue Mar 20th, 2018'),
-             StrToDate('Wed Mar 21st, 2018'), StrToDate('Thu Mar 22nd, 2018'),
-             StrToDate('Fri Mar 23rd, 2018'), StrToDate('Sat Mar 24th, 2018'),
-             StrToDate('Sun Mar 25th, 2018')])
+        self.assertListEqual(util.ParseDates('this week'), [
+            StrToDate('Mon Mar 19th, 2018'),
+            StrToDate('Tue Mar 20th, 2018'),
+            StrToDate('Wed Mar 21st, 2018'),
+            StrToDate('Thu Mar 22nd, 2018'),
+            StrToDate('Fri Mar 23rd, 2018'),
+            StrToDate('Sat Mar 24th, 2018'),
+            StrToDate('Sun Mar 25th, 2018'),
+        ])
 
     @freezegun.freeze_time('Mon Mar 26th, 2018 12pm')
     def testLastWeek(self):
-        self.assertListEqual(
-            util.ParseDates('last week'),
-            [StrToDate('Mon Mar 19th, 2018'), StrToDate('Tue Mar 20th, 2018'),
-             StrToDate('Wed Mar 21st, 2018'), StrToDate('Thu Mar 22nd, 2018'),
-             StrToDate('Fri Mar 23rd, 2018'), StrToDate('Sat Mar 24th, 2018'),
-             StrToDate('Sun Mar 25th, 2018')])
+        self.assertListEqual(util.ParseDates('last week'), [
+            StrToDate('Mon Mar 19th, 2018'),
+            StrToDate('Tue Mar 20th, 2018'),
+            StrToDate('Wed Mar 21st, 2018'),
+            StrToDate('Thu Mar 22nd, 2018'),
+            StrToDate('Fri Mar 23rd, 2018'),
+            StrToDate('Sat Mar 24th, 2018'),
+            StrToDate('Sun Mar 25th, 2018'),
+        ])
 
     @freezegun.freeze_time('Sat Mar 24th, 2018 12pm')
     def testTodayOnSaturdayRoundsToFridayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            util.ParseDates('today', workdays_only=True),
-            [StrToDate('Fri Mar 23rd, 2018')])
+        self.assertListEqual(util.ParseDates('today', workdays_only=True), [
+            StrToDate('Fri Mar 23rd, 2018'),
+        ])
 
     @freezegun.freeze_time('Sun Mar 25th, 2018 12pm')
     def testTodayOnSundayRoundsToFridayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            util.ParseDates('today', workdays_only=True),
-            [StrToDate('Fri Mar 23rd, 2018')])
+        self.assertListEqual(util.ParseDates('today', workdays_only=True), [
+            StrToDate('Fri Mar 23rd, 2018'),
+        ])
 
     @freezegun.freeze_time('Sat Mar 24th, 2018 12pm')
     def testWorkWeekReturnedInWorkdayOnlyMode(self):
-        self.assertListEqual(
-            util.ParseDates('this week', workdays_only=True),
-            [StrToDate('Mon Mar 19th, 2018'), StrToDate('Tue Mar 20th, 2018'),
-             StrToDate('Wed Mar 21st, 2018'), StrToDate('Thu Mar 22nd, 2018'),
-             StrToDate('Fri Mar 23rd, 2018')])
+        self.assertListEqual(util.ParseDates('this week', workdays_only=True), [
+            StrToDate('Mon Mar 19th, 2018'),
+            StrToDate('Tue Mar 20th, 2018'),
+            StrToDate('Wed Mar 21st, 2018'),
+            StrToDate('Thu Mar 22nd, 2018'),
+            StrToDate('Fri Mar 23rd, 2018'),
+        ])
 
     @freezegun.freeze_time('Sun Jan 17th, 1993 9pm')
     def testYesterdayOnSundayReturnsFridayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            [StrToDate('Fri Jan 15th, 1993')],
-            util.ParseDates('yesterday', workdays_only=True))
+        self.assertListEqual(util.ParseDates('yesterday', workdays_only=True), [
+            StrToDate('Fri Jan 15th, 1993'),
+        ])
 
     @freezegun.freeze_time('Mon Jan 18th, 1993 12pm')
     def testYesterdayOnMondayReturnsFridayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            [StrToDate('Fri Jan 15th, 1993')],
-            util.ParseDates('yesterday', workdays_only=True))
+        self.assertListEqual(util.ParseDates('yesterday', workdays_only=True), [
+            StrToDate('Fri Jan 15th, 1993'),
+        ])
 
     @freezegun.freeze_time('Fri Mar 23rd, 2018 12pm')
     def testTomorrowOnFridayReturnsMondayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            [StrToDate('Mon Mar 26th, 2018')],
-            util.ParseDates('tomorrow', workdays_only=True))
+        self.assertListEqual(util.ParseDates('tomorrow', workdays_only=True), [
+            StrToDate('Mon Mar 26th, 2018'),
+        ])
 
     @freezegun.freeze_time('Sat Mar 24th, 2018 12pm')
     def testTomorrowOnSaturdayReturnsMondayInWorkdaysOnlyMode(self):
-        self.assertListEqual(
-            [StrToDate('Mon Mar 26th, 2018')],
-            util.ParseDates('tomorrow', workdays_only=True))
+        self.assertListEqual(util.ParseDates('tomorrow', workdays_only=True), [
+            StrToDate('Mon Mar 26th, 2018'),
+        ])
 
 
 if __name__ == '__main__':
