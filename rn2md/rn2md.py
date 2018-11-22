df32ecb86b15cfcc87583576adbe72ba4fcb1d5a
Brian Rodriguez <brian@brianrodri.com>
2018-11-22 13:18:34 -0500

    Fix lint errors in rn2md

--- rn2md/rn2md.py
+++ rn2md/rn2md.py
@@ -1,13 +1,12 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 """TODO(brianrodri): Better module doc string."""
-import collections
 import sys
 
-import config
-import util
-import storage
-import transformers
+from . import config
+from . import util
+from . import storage
+from . import transformers
 
 
 def rn2md(lines):
@@ -23,23 +22,30 @@ def rn2md(lines):
     ]
     # Transformers are generators with meanginful state, so they need to be
     # prepared with an initial call to `next`.
-    for t in required_transformer_sequence:
-        next(t)
+    for transformer in required_transformer_sequence:
+        next(transformer, None)
     for line in lines:
-        for t in required_transformer_sequence:
-            line = t.send(line)
+        for transformer in required_transformer_sequence:
+            line = transformer.send(line)
         yield line
 
 
-if __name__ == '__main__':
-    options, remaining_argv = config.BuildConfigOptions(sys.argv)
-    daily_entries = storage.LoadDailyEntries(options.DataPath())
+def main():
+    """Business logic for rn2md utility."""
+    options, remaining_argv = config.build_config_options(sys.argv)
+    daily_entries = storage.LoadDailyEntries(options.data_path)
     target_date = ' '.join(remaining_argv) or 'today'
-    dates = util.ParseDates(target_date , workdays_only=options.WorkdaysOnly())
+    dates = util.ParseDates(target_date, workdays_only=options.workdays_only)
 
-    def FormatDate(date):
-        entry_lines = [date.strftime('# %a %b %d, %Y')]
-        entry_lines.extend(l.rstrip() for l in daily_entries[date].split('\n'))
+    def format_day_entry(date):
+        entry_lines = [l.rstrip() for l in daily_entries[date].split('\n')]
+        # Add header for date.
+        entry_lines.insert(0, date.strftime('# %a %b %d, %Y'))
         return '\n'.join(rn2md(entry_lines))
 
-    print('\n\n\n'.join(FormatDate(d) for d in dates if d in daily_entries))
+    print(
+        '\n\n\n'.join(format_day_entry(d) for d in dates if d in daily_entries))
+
+
+if __name__ == '__main__':
+    main()
