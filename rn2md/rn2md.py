c8f6b9d04386ca22fa8223cefe699979d67ed62a
Brian Rodriguez <brian@brianrodri.com>
2018-11-22 12:55:48 -0500

    Fix rn2md imports and usage

--- rn2md/rn2md.py
+++ rn2md/rn2md.py
@@ -4,14 +4,15 @@
 import collections
 import sys
 
-from . import config
-from . import util
-from . import storage
-from . import transformers
+import config
+import util
+import storage
+import transformers
 
 
-def RednotebookToMarkdown(day_entry_lines):
-    line_transformers = [
+def rn2md(lines):
+    """Transform given lines from Markdown syntax to RedNotebook syntax."""
+    mandatory_transformer_sequence = [
         transformers.InnerUnderscoreEscaper(),
         transformers.LinkTransformer(),
         transformers.HeaderTransformer(base_level=1),
@@ -20,28 +21,25 @@ def RednotebookToMarkdown(day_entry_lines):
         transformers.StrikethroughTransformer(),
         transformers.ListTransformer(),
     ]
-    for trans in line_transformers:
-        next(trans)  # Prepare to send lines to the transformers.
-    for line in day_entry_lines:
-        for trans in line_transformers:
-            line = trans.send(line)
+    # Transformers are generators, so they need to be prepared with an initial
+    # call to `next`.
+    for t in mandatory_transformer_sequence:
+        next(t)
+    for line in lines:
+        for t in mandatory_transformer_sequence:
+            line = t.send(line)
         yield line
 
 
-def main():
+if __name__ == '__main__':
     options, remaining_argv = config.BuildConfigOptions(sys.argv)
     daily_entries = storage.LoadDailyEntries(options.DataPath())
-    dates = util.ParseDates(
-        ' '.join(remaining_argv), workdays_only=options.WorkdaysOnly())
+    target_date =' '.join(remaining_argv) or 'today'
+    dates = util.ParseDates(target_date , workdays_only=options.WorkdaysOnly())
 
     def FormatDate(date):
-        day_entry_lines = [date.strftime('# %a %b %d, %Y')]
-        day_entry_lines.extend(
-            l.rstrip() for l in daily_entries[date].split('\n'))
-        return '\n'.join(RednotebookToMarkdown(day_entry_lines))
+        entry_lines = [date.strftime('# %a %b %d, %Y')]
+        entry_lines.extend(l.rstrip() for l in daily_entries[date].split('\n'))
+        return '\n'.join(rn2md(entry_lines))
 
     print('\n\n\n'.join(FormatDate(d) for d in dates if d in daily_entries))
-
-
-if __name__ == '__main__':
-    main()
