fe777963de61077bcc08b2bfb82a7ac1cdd985d4
Brian Rodriguez <brian@brianrodri.com>
2018-11-25 04:54:46 -0500

    simple requirements.txt loading

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 """Setup for rn2md package."""
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = [l.strip() for l in f.readlines() if l.strip()]
-
 setup(
     name='rn2md',
     version='0.1.0',
@@ -16,7 +13,7 @@ setup(
     author_email='brian@brianrodri.com',
     packages=find_packages(exclude=('tests',)),
     python_requires=">=3.5",
-    install_requires=requirements,
+    install_requires=open('requirements.txt').read().split('\n'),
     entry_points={
         'console_scripts': [
             'rn2md=rn2md.__main__:main',
