35abf34e4167f627ca46a33f1a4ab00fbfa18991
Brian Rodriguez <brian@brianrodri.com>
2018-11-25 06:06:29 -0500

    fix url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     license=open('LICENSE').read(),
-    url='https://github.com/brianrodri/RednotebookToMarkdown',
+    url='https://github.com/brianrodri/rn2md',
     author='Brian N. Rodriguez',
     author_email='brian@brianrodri.com',
     packages=find_packages(exclude=('tests',)),
