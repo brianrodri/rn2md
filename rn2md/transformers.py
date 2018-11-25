97b3a5e4c8d1c8b61e516d98cc08f9cb229156b8
Brian Rodriguez <brian@brianrodri.com>
2018-11-24 22:06:00 -0500

    Make all transformers into classes.

--- rn2md/transformers.py
+++ rn2md/transformers.py
@@ -32,25 +32,44 @@ class TransformerBase():
         return self._transformer.send(line)
 
 
-def LinkTransformer():  # pylint: disable=invalid-name
+class LinkTransformer(TransformerBase):
     """Makes coroutine to transform links from RedNotebook to Markdown."""
-    return _build_transformer(_link_transformer)
+    def _transformer_fun(self):
+        """Transforms '[[text ""url""]]' to '[text](url)'."""
+        line = ''
+        while True:
+            line = yield re.sub(r'\[([^\]]*?) ""(.*?)""\]', r'[\1](\2)', line)
 
 
-def ItalicTransformer():  # pylint: disable=invalid-name
+class ItalicTransformer(TransformerBase):
     """Makes coroutine to transform italics from RedNotebook to Markdown."""
-    return _build_transformer(_italic_transformer)
+    def _transformer_fun(self):
+        """Transforms '//text//' to '_text_'."""
+        line = ''
+        while True:
+            line = yield _sub_balanced_delims('//', '_', line)
 
 
-def StrikethroughTransformer():  # pylint: disable=invalid-name
+class StrikethroughTransformer(TransformerBase):
     """Makes coroutine to transform strikethroughs from RedNotebook to Markdown.
     """
-    return _build_transformer(_strikethrough_transformer)
+    def _transformer_fun(self):
+        """Transforms '--text--' to '**OBSOLETE**(text)'."""
+        line = ''
+        while True:
+            line = yield (
+                line if set(line) == {'-'} else
+                _sub_balanced_delims('--', ['**OBSOLETE**(', ')'], line,
+                                     data_op=lambda d: d.rstrip('.?!')))
 
 
-def CodeBlockTransformer():  # pylint: disable=invalid-name
+class CodeBlockTransformer(TransformerBase):
     """Makes coroutine to code blocks links from RedNotebook to Markdown."""
-    return _build_transformer(_code_block_transformer)
+    def _transformer_fun(self):
+        """Transforms codeblocks into markdown syntax."""
+        line = ''
+        while True:
+            line = yield _sub_balanced_delims('``', '`', line, preds=[_not_in_link])
 
 
 class HeaderTransformer(TransformerBase):
@@ -77,94 +96,46 @@ class HeaderTransformer(TransformerBase):
             line = ' '.join(['#' * (init_level + lvl), line[lvl:-lvl].lstrip()])
 
 
-def ListTransformer():  # pylint: disable=invalid-name
+class ListTransformer(TransformerBase):
     """Makes coroutine to transform underscores from RedNotebook to Markdown."""
-    return _build_transformer(_list_transformer)
-
-
-def InnerUnderscoreEscaper():  # pylint: disable=invalid-name
-    """Returns coroutine to transform links from RedNotebook to Markdown."""
-    return _build_transformer(_inner_underscore_escaper)
-
-
-def _build_transformer(transformer_fun, *args, **kwargs):
-    """Construct the given transformer.
-
-    Transformers are implemented via generators. We call `next` once before
-    returning them so they are ready to use.
-    """
-    transformer = transformer_fun(*args, **kwargs)
-    _ = next(transformer, None)
-    return transformer
-
-
-def _link_transformer():
-    """Transforms '[[text ""url""]]' to '[text](url)'."""
-    line = ''
-    while True:
-        line = yield re.sub(r'\[([^\]]*?) ""(.*?)""\]', r'[\1](\2)', line)
-
-
-def _italic_transformer():
-    """Transforms '//text//' to '_text_'."""
-    line = ''
-    while True:
-        line = yield _sub_balanced_delims('//', '_', line)
-
-
-def _strikethrough_transformer():
-    """Transforms '--text--' to '**OBSOLETE**(text)'."""
-    line = ''
-    while True:
-        line = yield (
-            line if set(line) == {'-'} else
-            _sub_balanced_delims('--', ['**OBSOLETE**(', ')'], line,
-                                 data_op=lambda d: d.rstrip('.?!')))
-
-
-def _list_transformer():
-    """Transforms ordered and unordered lists into markdown syntax."""
-    line = ''
-    ordered_list_history = defaultlist.defaultlist(lambda: 1)
-    sequential_empty_lines = 0
-    while True:
-        line = yield line
-        li_match = re.match(r'^\s*([-|\+])\s', line)
-        if li_match:
-            i = li_match.start(1)
-            # Always clear sub-items so they restart their numbering.
-            del ordered_list_history[i + 1:]
-            # Unordered lists do not use a different format in markdown.
-            if line[i] == '-':
-                continue
-            # Ordered lists should change to their actual number.
-            line = ''.join([
-                line[:i], str(ordered_list_history[i]) + '.', line[i + 1:]
-            ])
-            ordered_list_history[i] += 1
-        elif line.strip():
-            sequential_empty_lines = 0
-            ordered_list_history.clear()
-        else:
-            sequential_empty_lines += 1
-            if sequential_empty_lines >= 2:
+    def _transformer_fun(self):
+        """Transforms ordered and unordered lists into markdown syntax."""
+        line = ''
+        ordered_list_history = defaultlist.defaultlist(lambda: 1)
+        sequential_empty_lines = 0
+        while True:
+            line = yield line
+            li_match = re.match(r'^\s*([-|\+])\s', line)
+            if li_match:
+                i = li_match.start(1)
+                # Always clear sub-items so they restart their numbering.
+                del ordered_list_history[i + 1:]
+                # Unordered lists do not use a different format in markdown.
+                if line[i] == '-':
+                    continue
+                # Ordered lists should change to their actual number.
+                line = ''.join([
+                    line[:i], str(ordered_list_history[i]) + '.', line[i + 1:]
+                ])
+                ordered_list_history[i] += 1
+            elif line.strip():
+                sequential_empty_lines = 0
                 ordered_list_history.clear()
+            else:
+                sequential_empty_lines += 1
+                if sequential_empty_lines >= 2:
+                    ordered_list_history.clear()
 
 
-def _inner_underscore_escaper():
-    """Transforms underscores which need to be escaped."""
-    line = ''
-    while True:
-        line = yield line
-        for match in reversed(list(_filter_matches(r'(?<=\w)_(?=\w)', line))):
-            line = ''.join([line[:match.start()], r'\_', line[match.end():]])
-
-
-def _code_block_transformer():
-    """Transforms codeblocks into markdown syntax."""
-    line = ''
-    while True:
-        line = yield _sub_balanced_delims('``', '`', line, preds=[_not_in_link])
+class InnerUnderscoreEscaper(TransformerBase):
+    """Returns coroutine to transform links from RedNotebook to Markdown."""
+    def _transformer_fun(self):
+        """Transforms underscores which need to be escaped."""
+        line = ''
+        while True:
+            line = yield line
+            for match in reversed(list(_filter_matches(r'(?<=\w)_(?=\w)', line))):
+                line = ''.join([line[:match.start()], r'\_', line[match.end():]])
 
 
 def _sub_balanced_delims(delim_pattern, sub, string, data_op=str, **kwargs):
