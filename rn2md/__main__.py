a6b61ba697fcd924ad06f6472b63d3ab013dfde0
Brian Rodriguez <brian@brianrodri.com>
2018-12-06 11:38:05 -0500

    Remove useless whitespace

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -14,7 +14,6 @@ def main():
     date_arg = ' '.join(remaining_argv) or 'today'
     dates = util.parse_dates(date_arg, workdays_only=options.workdays_only)
     rednotebook = storage.load_rednotebook_entries(options.data_path)
-
     def rednotebook_to_markdown(date):
         """Returns the given date's RedNotebook entry in Markdown format."""
         rn_lines = rednotebook[date].split('\n') if date in rednotebook else []
@@ -28,18 +27,15 @@ def main():
             transformers.StrikethroughTransformer(),
             transformers.ListTransformer(),
         ]
-
         for rn_line in rn_lines:
             md_line = rn_line.rstrip()
             for rn2md_transformer in rn2md_transformers:
                 md_line = rn2md_transformer.fmt(md_line)
             md_lines.append(md_line)
-
         # Prepend entry with a markdown-formatted date header to help
         # distinguish it from other entries.
         md_lines.appendleft(date.strftime('# %a %b %d, %Y'))
         return '\n'.join(md_lines)
-
     print('\n\n\n'.join(rednotebook_to_markdown(d) for d in dates))
 
 
