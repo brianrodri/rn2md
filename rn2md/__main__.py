1db6dadd3dedeaf4448d45137d46f56b4c6d64ee
Brian Rodriguez <brian@brianrodri.com>
2018-12-07 03:35:26 -0500

    Just use line as arg

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -27,11 +27,11 @@ def main():
             transformers.StrikethroughTransformer(),
             transformers.ListTransformer(),
         ]
-        for rn_line in rn_lines:
-            md_line = rn_line.rstrip()
+        for line in rn_lines:
+            line = line.rstrip()
             for rn2md_transformer in rn2md_transformers:
-                md_line = rn2md_transformer.fmt(md_line)
-            md_lines.append(md_line)
+                line = rn2md_transformer.fmt(line)
+            md_lines.append(line)
         # Prepend entry with a markdown-formatted date header to help
         # distinguish it from other entries.
         md_lines.appendleft(date.strftime('# %a %b %d, %Y'))
