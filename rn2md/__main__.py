15768bd4cd81effea60a05397f87188e5a489513
Brian Rodriguez <brian@brianrodri.com>
2018-12-15 22:59:48 -0500

    use ternary if

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -11,9 +11,7 @@ def main():
     """Prints RedNotebook entries in markdown syntax."""
     config_options, remaining_argv = config.build_config_options(sys.argv)
     rednotebook = storage.load_rednotebook_entries(config_options.data_path)
-    date_arg = ' '.join(remaining_argv)
-    if not date_arg:
-        date_arg = 'today'
+    date_arg = ' '.join(remaining_argv) if remaining_argv else 'today'
     dates = util.parse_dates(date_arg, config_options.workdays_only)
     def rednotebook_to_markdown(date):
         """Returns the given date's RedNotebook entry in Markdown format."""
