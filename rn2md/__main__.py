185f5bce91de6caf3ab87662ca9836fdbf40220f
Brian Rodriguez <brian@brianrodri.com>
2018-12-15 20:56:41 -0500

    Use date_heading

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -16,11 +16,8 @@ def main():
     def rednotebook_to_markdown(date):
         """Returns the given date's RedNotebook entry in Markdown format."""
         rn_lines = rednotebook[date].split('\n') if date in rednotebook else []
-        # Entries begin with a date header to help distinguish them from each
-        # other. Subsequent headers are padded by 1 to render them as children.
-        md_lines = [date.strftime('# %a %b %d, %Y')]
-        formatter = formatters.RednotebookToMarkdownFormatter(header_padding=1)
-        md_lines.extend(formatter.send(line.rstrip()) for line in rn_lines)
+        formatter = formatters.RednotebookToMarkdownFormatter(date_heading=date)
+        md_lines = [formatter.send(line.rstrip()) for line in rn_lines]
         return '\n'.join(md_lines)
     print('\n\n\n'.join(rednotebook_to_markdown(d) for d in dates))
 
