49db77ae7f33e59aa1344468a4e75b4aeaf01df6
Brian Rodriguez <brian@brianrodri.com>
2019-01-24 10:22:56 -0500

    move date range fetching later

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -11,17 +11,17 @@ def main():
     """Prints RedNotebook entries in markdown syntax."""
     options, remaining_argv = config.Options.from_argv(sys.argv)
     rednotebook = storage.load_rednotebook_entries(options.data_path)
-    if remaining_argv:
-        date_range = util.parse_date_range(
-            ' '.join(remaining_argv), options.workdays_only)
-    else:
-        date_range = options.default_date_range
     def rednotebook_to_markdown(date):
         """Returns the given date's RedNotebook entry in Markdown format."""
         rn_lines = rednotebook[date].split('\n') if date in rednotebook else []
         formatter = formatters.format_rednotebook_as_markdown()
         md_lines = [formatter.send(line.rstrip()) for line in rn_lines]
         return '\n'.join(md_lines)
+    if remaining_argv:
+        date_range = util.parse_date_range(
+            ' '.join(remaining_argv), options.workdays_only)
+    else:
+        date_range = options.default_date_range
     print('\n\n\n'.join(rednotebook_to_markdown(d) for d in date_range))
 
 
