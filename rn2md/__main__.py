cdb9779d22bb21843651a9920e6fa11dded91785
Brian Rodriguez <brian@brianrodri.com>
2019-04-21 04:34:18 -0400

    better organized logic

--- rn2md/__main__.py
+++ rn2md/__main__.py
@@ -11,24 +11,22 @@ ENTRY_SEP = '\n\n\n'
 
 def main():
     """Prints RedNotebook entries in markdown syntax."""
-    opt, remaining_argv = config.Options.from_argv(sys.argv)
-    rednotebook = storage.load_rednotebook_entries(opt.data_path)
+    options, remaining_argv = config.Options.from_argv(sys.argv)
+    if remaining_argv:
+        date_range = (
+            util.parse_date_range(' '.join(remaining_argv), options.workdays_only))
+    else:
+        date_range = options.default_date_range
+
+    rednotebook = storage.load_rednotebook_entries(options.data_path)
     def rednotebook_to_markdown(date):
         """Returns the given date's RedNotebook entry in Markdown format."""
         rn_lines = rednotebook[date].split('\n') if date in rednotebook else []
         formatter = formatters.format_rednotebook_as_markdown()
         md_lines = [formatter.send(line.rstrip()) for line in rn_lines]
         return '\n'.join(md_lines)
-<<<<<<< HEAD
-    print(ENTRY_SEP.join(rednotebook_to_markdown(d) for d in dates))
-=======
-    if remaining_argv:
-        date_range = (
-            util.parse_date_range(' '.join(remaining_argv), opt.workdays_only))
-    else:
-        date_range = opt.default_date_range
-    print('\n\n\n'.join(rednotebook_to_markdown(d) for d in date_range))
->>>>>>> 164c4fbd55db7214ad8a0cf7585a6d4527b31d69
+
+    print(ENTRY_SEP.join(rednotebook_to_markdown(d) for d in date_range))
 
 
 if __name__ == '__main__':
